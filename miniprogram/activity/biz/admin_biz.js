eaaff8003647d71990f7e63b3df8ea425096126a
雷诺啊 <104705079+leinuoa111@users.noreply.github.com>
2024-10-08 08:56:56 +0800

    Update admin_biz.js

--- miniprogram/activity/biz/admin_biz.js
+++ miniprogram/activity/biz/admin_biz.js
@@ -1,7 +1,7 @@
 /**
  * Notes: 后台管理模块业务逻辑
  * Ver : CCMiniCloud Framework 2.0.1 ALL RIGHTS RESERVED BY www.code942.com
- * Date: 2020-11-14 07:48:00 
+ * Date: 2023-11-14 07:48:00 
  */
 
 const BaseCCMiniBiz = require('./base_ccmini_biz.js');
